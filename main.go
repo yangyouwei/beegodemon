2a65f36f37c1e4643e79d8d45d3fe1a8b5468cc9
dabai <yang_youwei81@163.com>
2019-08-09 15:50:16 +0800

    fix routers

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-	"github.com/yangyouwei/beegodemon/controllers"
 	"github.com/yangyouwei/beegodemon/models"
 	_ "github.com/yangyouwei/beegodemon/routers"
 )
@@ -15,7 +14,5 @@ func init()  {
 func main() {
 	orm.Debug = true
 	orm.RunSyncdb("default",false,true)  //默认是不会自动建表的。只建一次表
-	beego.Router("/",&controllers.HomeController{})
-	beego.Router("/login",&controllers.LoginController{})
 	beego.Run()
 }
